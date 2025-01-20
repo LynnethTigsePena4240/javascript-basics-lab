f23695464d39b90b49fd0468a263955dc5543986
LynnethTigsePena4240 <98134702+LynnethTigsePena4240@users.noreply.github.com>
2025-01-20 02:10:02 -0500

    Add files via upload
    uploading files (note still need to edit code to add comments of explaing what does what)

---
+++ script.js
@@ -0,0 +1,50 @@
+//declaring and using variables
+var studentName = "john"
+var studentAge = 21
+var isEnrolled = true
+
+console.log(studentName,studentName,isEnrolled);
+
+//demonstrating data type
+var animals = ["dog","cat","sloth"]
+
+var person ={
+    name: "smith",
+    age: 25,
+    lastName: "Mcdonalds" 
+}
+
+console.log(animals,person);
+
+//using basic operators
+let a = 5
+let b = 3
+//Arithmetic Operators:
+console.log(a+b);
+console.log(b-4);
+console.log(a*10);
+console.log(b/a);
+
+// Comparison Operators:
+console.log(a>b);
+console.log(a<b);
+console.log(b=a);
+console.log(a==b);
+console.log(a===a);
+console.log(b!=a);
+
+//Logical Operators:
+console.log(a&&b);
+console.log(a||b);
+console.log(!a);
+
+
+
+
+
+
+
+
+
+
+
